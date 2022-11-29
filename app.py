2d8655e4224c00527c41c2a4341a0480792c15c5
Dylan Harvey <dharveyasp@gmail.com>
2022-11-29 15:20:20 -0500

    Added inital files

---
+++ app.py
@@ -0,0 +1,37 @@
+# MTA0Mjg3OTcxOTg2NTA2NTU2Mg.GsFqGM.roDDhpVg1DgfJGLOlNy4zxV84Zr77OSekPiG1s
+import os
+
+import certifi
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+
+client.run(os.getenv('TOKEN'))
+# error getting ssl cert
+
+# ideas:
+# chat logging
+# chat cleanup
+# game? tictactoe? bingo game based on words in chat - dans idea?
+# reaction roles
